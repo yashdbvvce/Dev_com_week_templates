d60b35991cee5ac777cf75f1b73fd3fed812c035
karthik-s-v <vvce22cse0150@vvce.ac.in>
2025-04-16 14:12:15 +0530

    xyz

--- 2_Langchain/1_Chat_models/1_google_chat_model.py
+++ 2_Langchain/1_Chat_models/1_google_chat_model.py
@@ -1,4 +1,11 @@
-
 # Type this ===============================================
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Sample
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
+result = llm.invoke("Hello How are you ?")
+print(result.content)
 # Type this ===============================================
--- 2_Langchain/1_Chat_models/Exercise_1/your_code.py
+++ 2_Langchain/1_Chat_models/Exercise_1/your_code.py
@@ -6,4 +6,8 @@ load_dotenv()
 # Update this function
 def generate_response(user_input):
     # Your code goes here
-    return 0
+    # Type this ===============================================
+    llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
+    result = llm.invoke("Hello How are you ?")
+    return result.content
+    # Type this ===============================================
