5d30eeffe22cc9b4a801ce0080afa1917a5da1c7
karthik-s-v <vvce22cse0150@vvce.ac.in>
2025-04-16 14:38:49 +0530

    xyz

--- 2_Langchain/3_Prompt_Templates/1_Basic_Prompt_Template.py
+++ 2_Langchain/3_Prompt_Templates/1_Basic_Prompt_Template.py
@@ -6,14 +6,13 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
 
-
-# You code goes here
-TEMPLATE = ""
-prompt_template = ""
-
+# Type this ===============================================
+TEMPLATE = "Tell me a joke on {topic}"
+prompt_template = ChatPromptTemplate.from_template(TEMPLATE)
+# Type this ===============================================
 
 # Gives out a messages array
-prompt = prompt_template.invoke({"topic":"Cats"})
+prompt = prompt_template.invoke({"topic":"CSK"})
 print(prompt)
 response = llm.invoke(prompt)
 print(response.content)
